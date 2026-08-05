4281e788ecd9983cdf2a2f92c3014c3c05b104c5
Cody Milliron <codymilliron.dev@gmail.com>
2026-08-05 16:03:37 -0400

    refactoring

--- weekly_report.py
+++ weekly_report.py
@@ -1,21 +1,24 @@
-import datetime, sys
+import datetime
+import sys
 from pathlib import Path
-from config import output_folder, employees, cody_default_hours
+
+from config import cody_default_hours, employees, output_folder
 
 
 def get_hours_by_employee(employee: str, prev_sunday: datetime.date):
     """
-    Gets the number of hours the employee works each day. 
+    Gets the number of hours the employee works each day.
     It returns an array of hours with index 0 = Monday
     """
     print(f"Please enter your time for {employee}")
     work_week = []
     for i in range(1, 8):
-        day = prev_sunday - datetime.timedelta(days=(7-i))
-        current_day = f"{day.strftime('%Y-%m-%d')} - {day.strftime("%A")}: "
+        day = prev_sunday - datetime.timedelta(days=(7 - i))
+        current_day = f"{day.strftime('%Y-%m-%d')} - {day.strftime('%A')}: "
         hours = input(current_day)
         work_week.append(float(hours))
     return work_week
+
 
 def create_folder(dest_folder: Path):
     try:
@@ -41,13 +44,12 @@ def process_workweek(work_week, employee, prev_sunday) -> str:
     output = f"{employee} - For Week Ending on {prev_sunday.strftime('%m/%d/%Y')}\n\n"
     total = 0
     for ind, hours in enumerate(work_week, 1):
-        day = prev_sunday - datetime.timedelta(days=(7-ind))
-        current_day = f"{day.strftime('%Y-%m-%d')} - {day.strftime("%A") + ":":15} {hours:>4.2f}\n"
+        day = prev_sunday - datetime.timedelta(days=(7 - ind))
+        current_day = f"{day.strftime('%Y-%m-%d')} - {day.strftime('%A') + ':':15} {hours:>4.2f}\n"
         total += hours
         output += current_day
-    output += f"{"Work Week totals:":28} {total:>5.2f}"
+    output += f"{'Work Week totals:':28} {total:>5.2f}"
     return output
-
 
 
 def process_payroll():
@@ -70,21 +72,25 @@ def process_payroll():
     previous_sunday = today - datetime.timedelta(days=days_since_sunday)
 
     # Format the dates for use in paths and content
-    paydate_formated = upcoming_friday.strftime('%Y-%m-%d')
-    sunday_date_str = previous_sunday.strftime('%m/%d/%Y') # Using MM/DD/YYYY for content date
-    prev_sunday_string = previous_sunday.strftime('%Y-%m-%d')
+    paydate_formated = upcoming_friday.strftime("%Y-%m-%d")
+    sunday_date_str = previous_sunday.strftime(
+        "%m/%d/%Y"
+    )  # Using MM/DD/YYYY for content date
+    prev_sunday_string = previous_sunday.strftime("%Y-%m-%d")
 
     for employee in employees:
         hours = []
         if employee == "Cody Milliron":
             hours = cody_default_hours
         else:
-            hours = get_hours_by_employee(employee=employee, prev_sunday=previous_sunday)
+            hours = get_hours_by_employee(
+                employee=employee, prev_sunday=previous_sunday
+            )
         content = process_workweek(hours, employee, previous_sunday)
         write_file(employee, prev_sunday_string, paydate_formated, content)
 
     done = False
-    while (not done):
+    while not done:
         cont = input("Do you have any more employees to add (yes/no): ")
         if cont.lower() == "n" or cont.lower() == "no":
             done = True
